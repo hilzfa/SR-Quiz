544fd3c3cc6f43030f3ef7aa72e87f17122611c5
Fabi <fabian.hilz@t-online.de>
2016-03-30 23:20:13 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,28 @@
 var express = require('express');
 var app = express();
+app.use(express.static('../webui/'));
+var rootView = '../webui';
 
-app.use(express.static('../webui'));
+
+
+app.get('/',function(req,res){
+  console.log(reg);
+  res.sendFile('index.html', {root: rootView });
+
+});
+
+
+app.post('/',function(req,res){
+  console.log(req.params.username);
+  res.statusCode = 200;
+    res.end();
+
+});
+
+app.get('/main',function(req,res){
+  res.sendFile('/view/main.html', {root: rootView });
+});
+
 
 app.listen(8000);
-console.log("Server is now running...");
+console.log("Server is now running on port 8000");
